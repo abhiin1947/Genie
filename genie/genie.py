682b8f3b442a4fd7cbadc463806f1ef53e002259
Shrinidhi Sridhar <shrinidhisridhar30@gmail.com>
2017-04-29 13:47:09 -0400

    Basic layout-homepage
    Basic layout of the homepage designed

--- genie/genie.py
+++ genie/genie.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/map_locations')
+def map_locations():
+    return render_template('map-locations.html')
 
 if __name__ == '__main__':
     app.run()
