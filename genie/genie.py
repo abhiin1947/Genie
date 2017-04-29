273dba025be2b7eea930617b84eeae6f052823f7
Abhinandan Ramaprasath <abhiin1947@gmail.com>
2017-04-29 13:55:46 -0400

    Commits

--- genie/genie.py
+++ genie/genie.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+import urllib2
+import xmltodict
+import json
 
 app = Flask(__name__)
 
@@ -8,6 +11,22 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+
+@app.route('/get_mosquitos')
+def mosquitos():
+    response = urllib2.urlopen('http://www.boldsystems.org/index.php/API_Public/combined?bin=BOLD:AAA5125&format=xml')
+    res_string = response.read()
+    result = xmltodict.parse(res_string)
+    return json.dumps(result)
+
+
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 if __name__ == '__main__':
     app.run()
