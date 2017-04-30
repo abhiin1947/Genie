cef70bf2be9332fd6956976a32ec0069a2b2be5f
Abhinandan Ramaprasath <abhiin1947@gmail.com>
2017-04-30 14:22:19 -0400

    asdfasd

--- genie/genie.py
+++ genie/genie.py
@@ -5,6 +5,7 @@ from flask import jsonify
 import urllib2
 import xmltodict
 import difflib
+import heapq
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -55,6 +56,34 @@ def dict_record(x):
             "position": "left",
             "content": x["processid"]
         },
+        "nucleotide": x["sequences"]["sequence"]["nucleotides"],
+        "href": "http://www.boldsystems.org/index.php/Public_BarcodeCluster?clusteruri=" + x["bin_uri"]
+    }
+
+
+def dict_record_diff(x, main):
+    a = main["sequences"]["sequence"]["nucleotides"].replace("-", "").strip()
+    b = x["sequences"]["sequence"]["nucleotides"].replace("-", "").strip()
+
+    diff = []
+    for i, s in enumerate(difflib.ndiff(a, b)):
+        if s[0] == ' ':
+            continue
+        elif s[0] == '-':
+            diff.append(u'Delete {} from position {}'.format(s[-1], i))
+        elif s[0] == '+':
+            diff.append(u'Add {} to position {}'.format(s[-1], i))
+
+    return {
+        "id": str(x["_id"]),
+        "latitude": float(x["collection_event"]["coordinates"]["lat"]),
+        "longitude": float(x["collection_event"]["coordinates"]["lon"]),
+        "text": {
+            "position": "left",
+            "content": x["processid"]
+        },
+        "nucleotide": x["sequences"]["sequence"]["nucleotides"].replace("-", "").strip(),
+        "diff": diff,
         "href": "http://www.boldsystems.org/index.php/Public_BarcodeCluster?clusteruri=" + x["bin_uri"]
     }
 
@@ -70,7 +99,9 @@ def get_plots():
 
     abc = []
     for x in largest_bin:
-        if "collection_event" in x and "coordinates" in x["collection_event"] and "processid" in x and "sequences" in x and "sequence" in x["sequences"] and "nucleotides" in x["sequences"]["sequence"]:
+        if "collection_event" in x and "coordinates" in x[
+            "collection_event"] and "processid" in x and "sequences" in x and "sequence" in x[
+            "sequences"] and "nucleotides" in x["sequences"]["sequence"]:
             abc.append(dict_record(x))
 
     return jsonify({"plots": abc})
@@ -88,17 +119,39 @@ def get_relatives(count, ido):
 
         n = []
         for record in collection.find():
-            if "sequences" in record and str(record["_id"]) != ido and "collection_event" in record and "coordinates" in record["collection_event"] and "specimen_identifiers" in record and "catalognum" in record["specimen_identifiers"]\
-                    and "sequences" in record and "sequence" in record["sequences"] and "nucleotides" in record["sequences"]["sequence"]:
+            if "sequences" in record and str(record["_id"]) != ido and "collection_event" in record and "coordinates" in \
+                    record["collection_event"] and "specimen_identifiers" in record and "catalognum" in record[
+                "specimen_identifiers"] \
+                    and "sequences" in record and "sequence" in record["sequences"] and "nucleotides" in \
+                    record["sequences"]["sequence"]:
                 n.append(record["sequences"]["sequence"]["nucleotides"])
 
         c_neus = []
-        for closest in difflib.get_close_matches(c["sequences"]["sequence"]["nucleotides"], n, count, cutoff=0.4):
+        for closest in difflib.get_close_matches(c["sequences"]["sequence"]["nucleotides"], n, count, cutoff=0.2):
             c_neu = find_by_nucleotide(closest)
             c_neus.append(dict_record(c_neu))
 
         return jsonify({"closest": c_neus, "main": dict_record(c)})
     return jsonify({})
+
+
+def get_worst_matches(word, possibilities, n=3, cutoff=0.6):
+    if not n > 0:
+        raise ValueError("n must be > 0: %r" % (n,))
+    if not 0.0 <= cutoff <= 1.0:
+        raise ValueError("cutoff must be in [0.0, 1.0]: %r" % (cutoff,))
+    result = []
+    s = difflib.SequenceMatcher()
+    s.set_seq2(word)
+    for x in possibilities:
+        s.set_seq1(x)
+        if s.real_quick_ratio() >= cutoff >= s.quick_ratio() and s.ratio() <= cutoff:
+            result.append((s.ratio(), x))
+
+    # Move the best scorers to head of list
+    result = heapq.nsmallest(n, result)
+    # Strip scores for the best n matches
+    return [x for score, x in result]
 
 
 @app.route('/relatives/furthest/<int:count>/<ido>')
@@ -109,22 +162,21 @@ def get_relatives_furthest(count, ido):
 
         n = []
         for record in collection.find():
-            if "sequences" in record and record["_id"] != i and "collection_event" in record and "coordinates" in record["collection_event"] and "processid" in record and "sequences" in record and "sequence" in record["sequences"] and "nucleotides" in record["sequences"]["sequence"]:
+            if "sequences" in record and record["_id"] != i and "collection_event" in record and "coordinates" in \
+                    record["collection_event"] and "processid" in record and "sequences" in record and "sequence" in \
+                    record["sequences"] and "nucleotides" in record["sequences"]["sequence"]:
                 n.append(record["sequences"]["sequence"]["nucleotides"])
 
-        furthest = difflib.get_close_matches(c["sequences"]["sequence"]["nucleotides"], n, len(n), 1.0).reverse()
+        furthest = get_worst_matches(c["sequences"]["sequence"]["nucleotides"], n, count, cutoff=0.8)
         f_neus = []
-        o = 0
         for f in furthest:
             if f is not None:
                 f_neu = find_by_nucleotide(f)
                 f_neus.append(dict_record(f_neu))
-                o += 1
-                if o >= count:
-                    break
 
         return jsonify({"furthest": f_neus, "main": dict_record(c)})
     return jsonify({})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
